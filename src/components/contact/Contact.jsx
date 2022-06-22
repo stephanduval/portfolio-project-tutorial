61ded1bb2488d29309677dc27bf01c062ea435fe
Stephan DuVal <stephan.duval@gmail.com>
2022-06-22 13:03:44 -0400

    Finished all main components and main css file

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./contact.css";
 
-const contact = () => {
-  return <div>contact</div>;
+const Contact = () => {
+  return <div>Contact</div>;
 };
 
-export default contact;
+export default Contact;
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./testimondials.css";
+
+const Testimonials = () => {
+  return <div>Testimonials</div>;
+};
+
+export default Testimonials;
